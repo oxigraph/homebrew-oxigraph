b9b94e8fea6d9fe3c124477b4856a7fcb20bac27
Tpt <thomaspt@hotmail.fr>
2021-01-20 10:28:42 +0100

    Improves formulas

--- Formula/oxigraph.rb
+++ Formula/oxigraph.rb
@@ -1,18 +1,21 @@
 class Oxigraph < Formula
-  desc "A simple SPARQL database HTTP server"
+  desc "SPARQL database HTTP server"
   homepage "https://oxigraph.org/server/"
-  license any_of: ["Apache-2.0", "MIT"]
-  version "0.2.1"
   url "https://github.com/oxigraph/oxigraph/archive/v0.2.1.tar.gz"
   sha256 "2db897211ef7821f0988cf9140c843d4703aa92fbf8fcd9552ccc11843ae3ade"
+  license any_of: ["Apache-2.0", "MIT"]
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
 
-  def install
-    system "cargo", "build", "--release", "--bin", "oxigraph_server"
-    bin.install "target/release/oxigraph_server"
+  on_linux do
+    depends_on "openssl@1.1"
   end
-end
 
-test do
-  assert_match "oxigraph_server", shell_output("#{bin}/oxigraph_server --help")
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "server"
+  end
+
+  test do
+    assert_match "oxigraph_server", shell_output("#{bin}/oxigraph_server --help")
+  end
 end
